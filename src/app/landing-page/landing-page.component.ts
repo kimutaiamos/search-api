e9bec5a656d088216917c23386935bb6bdec3fd0
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-20 14:42:13 +0300

    added the page users ts file

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+ 
+  users!:User[];
 
-  constructor() { }
+  searchUserName(username: string){
+   this.httpServiceService.findUser(username).then(
+     ()=>{
+       this.users = this.httpServiceService.users;
+     },
+     (error: any)=>{
+     }
+   )
+  }
+  constructor(public httpServiceService:HttpService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
+    this.searchUserName('Ammoh-Moringa');
   }
 
 }
